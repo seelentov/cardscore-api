3a89079d6af0ba4467af9b3780f1565ac46dca0d
vladisluw <54843319+seelentov@users.noreply.github.com>
2024-07-25 06:30:12 +0500

    123

--- Program.cs
+++ Program.cs
@@ -59,7 +59,7 @@ builder.Services.AddSingleton<AsyncService>();
 
 
 builder.Services.AddHostedService<BackupService>(); 
-builder.Services.AddHostedService<NotificationWorkerService>();
+/*builder.Services.AddHostedService<NotificationWorkerService>();*/
 builder.Services.AddHostedService<GamesWorkerService>();
 
 builder.Host.ConfigureLogging(opt =>
