699865b46eb29a1a67ef9197cbcdac829dbed64b
vladisluw <54843319+seelentov@users.noreply.github.com>
2024-09-21 02:27:56 +0500

    Update SeleniumService.cs

--- Services/SeleniumService.cs
+++ Services/SeleniumService.cs
@@ -14,23 +14,24 @@ namespace cardscore_api.Services
         {
             ChromeOptions options = new ChromeOptions();
 
-/*            options.AddUserProfilePreference("profile.default_content_settings.images", 2); // Блокирует изображения
-            options.AddUserProfilePreference("profile.default_content_settings.stylesheets", 2); // Блокирует стили
-            options.AddUserProfilePreference("profile.managed_default_content_settings.images", 2); // Блокирует изображения
-            options.AddUserProfilePreference("profile.managed_default_content_settings.stylesheets", 2); // Блокирует стили
-            options.AddUserProfilePreference("cache.disk_cache_size", 0); // Отключает кэширование на диске
-            options.AddUserProfilePreference("cache.memory_cache_size", 0); // Отключает кэширование в памяти
-            options.AddUserProfilePreference("cache.enable", false); // Отключает кэширование
-            options.AddUserProfilePreference("extensions.enabled", false); // Отключает расширения
-            options.AddUserProfilePreference("privacy.clear_browsing_data_on_exit", true); // Очистка данных при выходе
-
+            /*            options.AddUserProfilePreference("profile.default_content_settings.images", 2); // Блокирует изображения
+                        options.AddUserProfilePreference("profile.default_content_settings.stylesheets", 2); // Блокирует стили
+                        options.AddUserProfilePreference("profile.managed_default_content_settings.images", 2); // Блокирует изображения
+                        options.AddUserProfilePreference("profile.managed_default_content_settings.stylesheets", 2); // Блокирует стили
+                        options.AddUserProfilePreference("cache.disk_cache_size", 0); // Отключает кэширование на диске
+                        options.AddUserProfilePreference("cache.memory_cache_size", 0); // Отключает кэширование в памяти
+                        options.AddUserProfilePreference("cache.enable", false); // Отключает кэширование
+                        options.AddUserProfilePreference("extensions.enabled", false); // Отключает расширения
+                        options.AddUserProfilePreference("privacy.clear_browsing_data_on_exit", true); // Очистка данных при выходе
+                          options.AddArgument("--disable-features=NetworkService");
+              */
             options.AddArgument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36");
             options.AddArgument("--headless=new");
             options.AddArgument("disable-gpu");
               options.AddArgument("no-sandbox");
               options.AddArgument("--disable-extensions");
               options.AddArgument("--disable-features=NetworkService");
-  */
+
             _driverOptions = options;
             _driverOptions.PageLoadStrategy = PageLoadStrategy.Eager;
 
