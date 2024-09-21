85b5b13692c90740861152e2fe54d9ae3eb0d6be
vladisluw <54843319+seelentov@users.noreply.github.com>
2024-09-21 17:23:50 +0500

    Update SeleniumService.cs

--- Services/SeleniumService.cs
+++ Services/SeleniumService.cs
@@ -43,7 +43,7 @@ namespace cardscore_api.Services
               */
             options.AddArgument("no-sandbox");
 
-            options.BinaryLocation = "/usr/bin/google-chrome";
+            options.BinaryLocation = "/usr/bin/google-chrome-stable";
 
             _driverOptions = options;
 
